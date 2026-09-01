7039a453ce638f53a759a782bf6e39e7b20d8a88
Wasu9 <manoharstudio9@gmail.com>
2026-09-01 11:32:47 +0530

    fix: use default renderer in hyperreal office wrapper

--- src/components/OfficeSimulation3DHyperReal.tsx
+++ src/components/OfficeSimulation3DHyperReal.tsx
@@ -1,7 +1,6 @@
 import React from'react';
-import{OfficeSimulation3DGameV2}from'./OfficeSimulation3DGameV2';
+import OfficeSimulation3DGameV2 from'./OfficeSimulation3DGameV2';
 import{AgentDefinition,TaskRecord}from'../types';
 interface Props{agents:AgentDefinition[];activeTask:TaskRecord|null}
-/** Game-like procedural 3D office with third-person movement and animated NPC employees. */
 export const OfficeSimulation3DHyperReal:React.FC<Props>=(props)=><OfficeSimulation3DGameV2 {...props}/>;
 export default OfficeSimulation3DHyperReal;
